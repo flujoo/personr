5f660636c3f7776c06564405bbd434dc245b8cf4
flujoo <tnerrotana@gmail.com>
2020-05-25 22:36:31 +0400

    Define Shiny app

---
+++ R/app.R
@@ -0,0 +1,59 @@
+# make the package a Shiny app
+
+# wrap scale up in HTML
+htmlize_scale <- function(statement_number) {
+  # scale labels
+  sls <- test$scale$label
+  # scale values
+  svs <- test$scale$value
+  # scale length
+  n <- nrow(test$scale)
+  # wrap scale
+  s_html <- list()
+  for (i in 1:n) {
+    s_i <- shiny::tags$li(
+      # input element
+      shiny::tags$input(
+        type = "radio",
+        id = paste("value", i, sep = "_"),
+        name = paste("item", statement_number, sep = "_"),
+        value = svs[i]
+      ),
+      # label element
+      shiny::tags$label(sls[i], "for" = paste("value", i, sep = "_"))
+    )
+    s_html[[i]] <- s_i
+  }
+  # ordered list element
+  shiny::tags$ol(s_html, class = "scale")
+}
+
+
+# wrap test items up in HTML
+htmlize_items <- function() {
+  # all statements
+  ss <- test$statements
+  # total statement number
+  n <- length(ss)
+  # wrapped up items
+  items <- list()
+  for (i in 1:n) {
+    item_i <- shiny::div(
+      # wrap statement
+      shiny::p(ss[i], class = "statement"),
+      # wrap scale
+      htmlize_scale(i),
+      class = "item",
+      id = paste("item", i, sep = "_")
+    )
+    items[[i]] <- item_i
+  }
+  shiny::tags$main(items)
+}
+
+
+# launch Shiny app to start personality test
+launch_test <- function() {
+  path <- system.file("test", package = "personr")
+  shiny::runApp(appDir = path, launch.browser = TRUE)
+}
---
+++ inst/test/ui.R
@@ -0,0 +1,3 @@
+shiny::htmlTemplate(
+  system.file("test", "test.html", package = "personr")
+)
