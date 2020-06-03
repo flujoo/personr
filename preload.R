b6434a8fc25dfbaceede7ec2cc7a4142a32b2dda
flujoo <tnerrotana@gmail.com>
2020-06-03 13:11:22 +0400

    Update preload.R

--- preload.R
+++ preload.R
@@ -1,29 +1,18 @@
 # preload this file to
 # 1. compile style.scss
 # 2. use data in data-raw/
-# 3. finally, devtools::load_all()
 
 # in dir "personr"
 # source("preload.R")
 
-
 # compile style.scss
 system("sass style.scss inst/app/www/style.css")
 
+# use functions from personr to process data
+devtools::load_all()
 
 # use data in data-raw/
 source("data-raw/test.R", chdir = TRUE)
 source("data-raw/norm.R", chdir = TRUE)
-
-usethis::use_data(
-  test, score, score.numeric, score.data.frame, norm, plots,
-  internal = TRUE,
-  overwrite = TRUE
-)
-
+usethis::use_data(test, norm, internal = TRUE, overwrite = TRUE)
 rm(list = ls())
-
-
-devtools::load_all()
-launch_test("~/Desktop/")
-report(rep(1, 50), "~/Desktop/")
